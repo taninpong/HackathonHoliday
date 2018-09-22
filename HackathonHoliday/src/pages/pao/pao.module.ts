f1ef4ffbe47ba9cd4268babc1e0ead04102f2e68
thaninpong <thaninpong@perfenterprise.com>
2018-09-22 14:12:07 +0700

    create profile pao
    Signed-off-by: thaninpong <thaninpong@perfenterprise.com>

---
+++ HackathonHoliday/src/pages/pao/pao.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { PaoPage } from './pao';
+
+@NgModule({
+  declarations: [
+    PaoPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(PaoPage),
+  ],
+})
+export class PaoPageModule {}
